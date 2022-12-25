76740197e35713d116b493e1a6836716608b52ff
Sweetcheeks12354 <goodrichnik@gmail.com>
2022-12-25 14:27:08 -0500

    Program now works running itself again

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 import pyperclip
 import os
-import keyboard
+import keyboard as kyb
 import win32clipboard
 import time
 import xlwings as xw
@@ -10,9 +11,19 @@ import requests
 import pandas as pd
 import json
 import tkinter as tk
+import webbrowser
+import sys
+import psutil
+import logging
+import pynput
 
+#global variables
+g_root = None
+translation = None
 
 def on_ctrl_c():
+    
+    
     # Wait for the clipboard to be populated
     time.sleep(0.1)
     # Open the clipboard
@@ -25,8 +36,17 @@ def on_ctrl_c():
     # Close the clipboard
     win32clipboard.CloseClipboard()
   # Write the clipboard contents to the CSV file
-    write_to_csv(data)
-    
+    data = write_to_csv(data)
+    # set global translation variable to data
+    global translation
+    translation = data
+    #clear the clipboard
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.CloseClipboard()
+    #clear the keyboard listener
+    print('On Ctrl C done')
+    return None
     
 
 
@@ -36,21 +56,22 @@ def write_to_csv(data):
     if not os.path.isfile(cwd + '/data.csv'):
         
         df = pd.DataFrame(columns=['data'])
-        df.to_csv('data.csv', index=False)
+        df.to_csv('data.csv', index=False, encoding='utf-8')
     #append the data to the csv file in the first column last row
     
-    df = pd.read_csv('data.csv')
+    df = pd.read_csv('data.csv', encoding='utf-8')
     
     spanish = deepl_translate(data)
     #append the spanish translation to the csv file in the second column last row
     df = df.append({'english': data, 'spanish': spanish}, ignore_index=True)
     
     sp_en_response = [data, spanish]
-    df.to_csv('data.csv', index=False)
-    show_strings(sp_en_response)
+    df.to_csv('data.csv', index=False, encoding = 'utf-8')
+    return sp_en_response
+    
     
 def deepl_translate(data):
-    api_key = 'API KEY HERE'
+    api_key = 'b86af888-a93c-5c99-9e3e-684ddedf564d:fx'
     #take the data and translate it into Spanish
     url = 'https://api-free.deepl.com/v2/translate'
     params = {'auth_key': api_key, 'text': data, 'target_lang': 'ES'}
@@ -59,34 +80,74 @@ def deepl_translate(data):
     json_text = json.loads(response.text)
     response_Text = json_text['translations'][0]['text']
     return response_Text
-        
-
-import tkinter as tk
-
-def show_strings(strings):
-    root = tk.Tk()
-    root.title("Translation")
-    root.attributes('-topmost', True)
-    font = ('Helvetica', 36, 'bold')
-
-    label1 = tk.Label(root, text=strings[0], font=font)
-    label1.pack()
-    label2 = tk.Label(root, text=strings[1], font = font)
-    label2.pack()
-
-    root.mainloop()
-
-# Example usage
-
+     
 
     
 def main():
+    def open_website():
+        webbrowser.open("https://www.deepl.com/translator")
+    
+    def close_window():
+        #end the program and then restart it
+        #close window
+        
+        root.destroy()
+    
+    def on_press(key):
+        #pynput.keyboard.Key for q
+        if key == pynput.keyboard.Key.esc:
+            close_window()
+    
     # Set up a keyboard shortcut to trigger the on_ctrl_c function
     # when Ctrl + C is pressed
-    keyboard.add_hotkey('ctrl+c', on_ctrl_c)
-    # Keep the program running
-    keyboard.wait()
+    #close any Tkinter root windows that are open
+        
+    #clear any keyboard listeners that are open
+    
+    print('Starting program')
+    kyb.add_hotkey('ctrl+c', on_ctrl_c)
+    # Wait for the user to press Ctrl + C
+    kyb.wait('ctrl+c')
+    
+        
+    # Create the Tkinter GUI
+    root = tk.Tk()
+    root.title("Translation")
+    root.attributes('-topmost', True)
+    font = ('Helvetica', 26, 'bold')
+
+    label1 = tk.Label(root, text=translation[0], font=font)
+    label1.pack()
+    label2 = tk.Label(root, text=translation[1], font=font)
+    label2.pack()
+    label3 = tk.Label(root, text="Press 'q' to close", font=font)
+    label3.pack()
+
+    button = tk.Button(root, text="Go to website", font=font, command=open_website)
+    button.pack()
+
+    # Start the keyboard listener
+    listener = pynput.keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    # Start the Tkinter GUI's main loop
+    root.mainloop()
+
+    # Stop the keyboard listener
+    listener.stop()
+    try:
+        kyb.unhook_all()
+    except:
+        pass
+    try:
+        kyb.unhook_all_hotkeys()
+    except:
+        pass        
 
 
 if __name__ == '__main__':
-    main()
+   main()
+   os.execv(sys.executable, ['python'] + sys.argv)
+    
+
+
